7580fdbc764b5d52f80b0defe01e718ba4300e77
cybershard <ashwatdd01@gmail.com>
2021-01-06 07:48:57 -0500

    Formatting

--- dropship-native-addon/index.js
+++ dropship-native-addon/index.js
@@ -1,7 +1,15 @@
 const addon = require('bindings')('dropship_native_addon');
-
-try {
-    console.log(addon.parse("./Example.dll"));
-} catch (e) {
-    console.warn(e);
-}
+const os = require('os')
+const path = require('path')
+const parse = (userpath) => {
+    if (os.platform() === 'win32') {
+        const dllPath = path.join(__dirname, "Dropship.Native.dll")
+        return addon.parse(userpath, dllPath)
+    } else if (os.platform() === 'linux') {
+        const soPath = path.join(__dirname, "Dropship.Native.so")
+        return addon.parse(userpath, soPath)
+    } else {
+        throw new Error("Unsupported OS for Dropship.Native")
+    }
+};
+exports.parse = parse
--- dropship-native-addon/main.cpp
+++ dropship-native-addon/main.cpp
@@ -5,50 +5,45 @@ using namespace Napi;
 #ifdef WINDOWS_SPECIFIC_DEFINE
 #include <windows.h>
 
-void *dlopen (const char *filename, int flags)
-{
-    HINSTANCE hInst;
+void *dlopen(const char *filename, int flags) {
+  HINSTANCE hInst;
 
-    hInst= LoadLibraryA(filename);
-    if (hInst==NULL) {
-        return NULL;
-    }
-    return hInst;
+  hInst = LoadLibraryA(filename);
+  if (hInst == NULL) {
+    return NULL;
+  }
+  return hInst;
 }
 
-int dlclose (void *handle)
-{
-    BOOL ok;
-    int rc= 0;
+int dlclose(void *handle) {
+  BOOL ok;
+  int rc = 0;
 
-    ok= FreeLibrary ((HINSTANCE)handle);
-    if (! ok) {
-        rc= -1;
-    }
-    return rc;
+  ok = FreeLibrary((HINSTANCE)handle);
+  if (!ok) {
+    rc = -1;
+  }
+  return rc;
 }
 
-void *dlsym (void *handle, const char *name)
-{
-    FARPROC fp;
+void *dlsym(void *handle, const char *name) {
+  FARPROC fp;
 
-    fp= GetProcAddress ((HINSTANCE)handle, name);
-    if (!fp) {
-      return NULL;
-    }
-    return (void *)(intptr_t)fp;
+  fp = GetProcAddress((HINSTANCE)handle, name);
+  if (!fp) {
+    return NULL;
+  }
+  return (void *)(intptr_t)fp;
 }
 
-#define PARSING_LIB "./Dropship.Native.dll"
-#endif 
+#endif
 
 #ifdef LINUX_DEFINE
 #include <dlfcn.h>
-#define PARSING_LIB "./Dropship.Native.so"
 #endif
 
-struct ModMetadata
-{
+
+struct ModMetadata {
   bool success;
 
   char *assemblyName;
@@ -58,74 +53,77 @@ struct ModMetadata
   char *description;
   char *authors;
 
-  ModMetadata(bool s, char *description): success(s), description(description) {}
+  ModMetadata() {}
+  ModMetadata(bool s, char *description)
+      : success(s), description(description) {}
 };
 
-ModMetadata *parse(char *file)
-{
+bool parse(char *file, char *libpath, ModMetadata *metadata) {
   typedef ModMetadata *(*parse_t)(char *);
-  char *error;
-  
-  void *handle = dlopen(PARSING_LIB, 1);
-  
-  if (!handle)
-  {
-    ModMetadata metadata(false, "Could not find native parsing library.");
-    return &metadata;
+
+  void *handle = dlopen(libpath, 1);
+
+  if (!handle) {
+    metadata->success = false,
+    metadata->description = (char *)"Could not find native parsing library.";
+    return false;
   }
 
-  parse_t func = (parse_t) dlsym(handle, "parse");
-  if (!func)
-  {
-    ModMetadata metadata(false, "Could not locate parse function in native library.");
-    return &metadata;
+  parse_t func = (parse_t)dlsym(handle, "parse");
+  if (!func) {
+    metadata->success = false,
+    metadata->description = (char *)"Could not locate parse function in library.";
+    return false;
   }
 
-  ModMetadata *result = (*func)(file);
+  *metadata = *((*func)(file));
   dlclose(handle);
-  return result;
+  return true;
 }
 
-Value Parse(const CallbackInfo &info)
-{
+Value Parse(const CallbackInfo &info) {
   Env env = info.Env();
 
-  if (info.Length() != 1)
-  {
-    TypeError::New(env, "Wrong number of arguments").ThrowAsJavaScriptException();
+  if (info.Length() != 2) {
+    TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
     return env.Null();
   }
 
-  if (!info[0].IsString())
-  {
+  if (!info[0].IsString()) {
+    TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  if (!info[1].IsString()) {
     TypeError::New(env, "Wrong arguments").ThrowAsJavaScriptException();
     return env.Null();
   }
 
   char *file = strdup(info[0].As<String>().Utf8Value().c_str());
+  char *libpath = strdup(info[1].As<String>().Utf8Value().c_str());
 
-  ModMetadata *metadata = parse(file);
+  ModMetadata metadata{};
+  parse(file, libpath, &metadata);
 
-  if (!metadata->success)
-  {
-    Error::New(env, metadata->description).ThrowAsJavaScriptException();
+  if (!metadata.success) {
+    Error::New(env, metadata.description).ThrowAsJavaScriptException();
     return env.Null();
   }
 
   Object returnValue = Object::New(env);
 
-  returnValue.Set("assemblyName", String::New(env, metadata->assemblyName));
-  returnValue.Set("id", String::New(env, metadata->id));
-  returnValue.Set("name", String::New(env, metadata->name));
-  returnValue.Set("version", String::New(env, metadata->version));
-  returnValue.Set("description", String::New(env, metadata->description));
-  returnValue.Set("authors", String::New(env, metadata->authors));
+  returnValue.Set("assemblyName", String::New(env, metadata.assemblyName));
+  returnValue.Set("id", String::New(env, metadata.id));
+  returnValue.Set("name", String::New(env, metadata.name));
+  returnValue.Set("version", String::New(env, metadata.version));
+  returnValue.Set("description", String::New(env, metadata.description));
+  returnValue.Set("authors", String::New(env, metadata.authors));
 
   return returnValue;
 }
 
-Object Init(Env env, Object exports)
-{
+Object Init(Env env, Object exports) {
   exports.Set(String::New(env, "parse"), Function::New(env, Parse));
 
   return exports;
